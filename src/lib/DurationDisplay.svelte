337794a71df104bd350c8ab714895d016a8548ba
Brian Burns <BrianRBurns@gmail.com>
2024-08-28 17:39:46 -0400

    First pass at making the script counter page and components

---
+++ src/lib/DurationDisplay.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import type { Duration } from "./types";
+
+  export let duration: Duration;
+</script>
+
+{#if duration.hours > 0}
+  {duration.hours} {duration.hours > 1 ? "hours" : "hour"}
+{/if}
+{#if duration.minutes > 0}
+  {duration.minutes} {duration.minutes > 1 ? "minutes" : "minute"}
+{/if}
+{#if duration.seconds > 0}
+  {duration.seconds} {duration.seconds > 1 ? "seconds" : "second"}
+{/if}
---
+++ src/lib/Kounter.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import { fade, fly } from "svelte/transition";
+  import DurationDisplay from "./DurationDisplay.svelte";
+  import type { Duration } from "./types";
+
+  let text: string = "";
+  let duration: Duration = { hours: 0, minutes: 0, seconds: 0 };
+  $: {
+    let seconds = text.length / 10;
+    duration.hours = Math.floor(seconds / 3600);
+    seconds = seconds - duration.hours * 3600;
+    duration.minutes = Math.floor(seconds / 60);
+    seconds = seconds - duration.minutes * 60;
+    duration.seconds = Math.ceil(seconds);
+  }
+</script>
+
+<textarea placeholder="Enter a skrypt" bind:value={text} rows="10" cols="50"
+></textarea>
+<div class="container">
+  {#if text.length > 0}
+    <div in:fade out:fly={{ y: 40, duration: 250 }} class="duration-message">
+      This skrypt will take
+      <strong>
+        <DurationDisplay {duration} />
+      </strong>
+      to read
+    </div>
+  {:else}
+    <div in:fade out:fly={{ y: 40, duration: 250 }} class="empty-message">
+      Enter you skrypt to find out how long it will take to read
+    </div>
+  {/if}
+</div>
+
+<style>
+  .container {
+    display: grid;
+  }
+
+  .duration-message,
+  .empty-message {
+    grid-column: 1/2;
+    grid-row: 1/2;
+  }
+</style>
