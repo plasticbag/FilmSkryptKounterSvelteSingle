d55f82d71c45f32dee178413211ce52a9662fc30
Brian Burns <BrianRBurns@gmail.com>
2024-09-03 17:14:40 -0400

    Add single file build config and packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
-})
+export default defineConfig(({ command }) => ({
+  plugins: [
+    svelte(),
+    command === 'build' &&
+    viteSingleFile({
+      removeViteModuleLoader: true
+    })
+  ],
+  build: {
+    minify: true
+  }
+}));
