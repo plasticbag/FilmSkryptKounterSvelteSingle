4f973c1e2794543d6fa3ac25edecce5ffe7999ff
Brian Burns <BrianRBurns@gmail.com>
2024-09-24 14:28:58 -0400

    Update the test so that it passes and reflects the original intended logic

--- src/lib/Kounter.test.ts
+++ src/lib/Kounter.test.ts
@@ -12,12 +12,21 @@ describe("Sentances should take time, not exactly sure how much time, but time."
         //  enter our text
         await userEvent.type(input, "Yabba Dabba Doo, I'm coming for you.");
         //  get the output element
-        // const output = screen.getByText(/This skrypt will take .* to read/);
         const output = screen.getByRole("strong");
 
+        //  Check that we have something we expect. The time is put in a strong tag
         expect(output).toBeDefined();
 
-        // expect(seconds).toBeGreaterThan(2);
+        //  Get the innerHTML and parse out the seconds. This could be more clever
+        const content = output.innerHTML ?? "";
+        const match = /\s*([0-9]+) seconds/.exec(content);
+        const seconds = match?.[1] ?? "";
+
+        //  Debugging is hard...
+        // expect(JSON.stringify(match)).toBe("");
+
+        //  Test that the number of seconds is greater than 2
+        expect(parseInt(seconds)).toBeGreaterThan(2);
     });
 
 });
